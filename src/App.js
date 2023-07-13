af556cd730e871cfb92d74c553608b4a05bd49ae
dark_dip2 <diptya99@gmail.com>
2023-07-13 14:53:51 +0530

    Weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from "react"
-import './App.css';
-import Navbar from './components/Navbar'
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
+
   return (
-    <>
-      <Navbar />
-      <Routes> 
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/rooms" element={ <Rooms/>}/>
-        <Route exact path="/rooms/:slug" element={<SingleRoom/>}/>     
-        <Route path="*" element={ <Error/>}/>
-      </Routes>
-    </>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
   );
 }
 
